c5c129f14f7122711c778adedad6ca0c8a324629
atharv magdum <atharvmagdum1008@gmail.com>
2025-12-10 23:24:34 +0530

    Add Subtract

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import arithmetic as arith
 print("Hello World !")
+
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter Second Number :"))
+
+arith.add(num1, num2)
+arith.sub(num1, num2)
